1fa18115d24cde7deee8870ec914e291a74e48df
tbvns <69420062+tbvns25565@users.noreply.github.com>
2023-07-06 20:08:02 +0000

    Movement added (Jumping not working)

---
+++ UnityGameTest/Assets/PlayerMovement.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public CharacterController controller;
+    public float speed = 12f;
+    public float Gravity = -9.81f;
+    Vector3 Velocity;
+    public Transform groundCheck;
+    public float GroundDistance = 0.4f;
+    public LayerMask groundMask;
+    bool isGrounded;
+    public float JumpHeight = 3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, GroundDistance, groundMask);
+
+        if (isGrounded && Velocity.y < 0) { 
+            Velocity.y = -2f;
+        }
+
+        float X = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * X + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
+
+        Velocity.y = Gravity * Time.deltaTime;
+        controller.Move(Velocity * Time.deltaTime);
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            Velocity.y = Mathf.Sqrt(JumpHeight * -2f * Gravity);
+            controller.Move(Velocity * Time.deltaTime);
+        }
+
+    }
+}
