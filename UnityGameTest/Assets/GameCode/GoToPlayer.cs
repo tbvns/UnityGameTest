48c0d1e69a4b0ec4073f329fdce7334caa0ce450
tbvns <69420062+tbvns25565@users.noreply.github.com>
2023-07-14 12:05:46 +0000

    Detect and stop the game if too close

--- UnityGameTest/Assets/GameCode/GoToPlayer.cs
+++ UnityGameTest/Assets/GameCode/GoToPlayer.cs
@@ -5,6 +5,8 @@ using UnityEngine.AI;
 
 public class GoToPlayer : MonoBehaviour
 {
+    public Transform Check;
+    public LayerMask mask;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,5 +18,11 @@ public class GoToPlayer : MonoBehaviour
     {
         NavMeshAgent agent = GetComponent<NavMeshAgent>();
         agent.destination = GameObject.Find("Player").transform.position;
+
+        bool isnear = Physics.CheckSphere(Check.position, 2, mask);
+        if (isnear)
+        {
+            Debug.Break();
+        }
     }
 }
