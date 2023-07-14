d90d927d4b53948cdfec46ff839a28822556ab06
tbvns <69420062+tbvns25565@users.noreply.github.com>
2023-07-14 12:35:15 +0000

    Added death

--- UnityGameTest/Assets/GameCode/GoToPlayer.cs
+++ UnityGameTest/Assets/GameCode/GoToPlayer.cs
@@ -2,15 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.SceneManagement;
 
 public class GoToPlayer : MonoBehaviour
 {
+    public float countdown;
+    public bool asbeenjumpscared;
+    public GameObject Jumpscare;
     public Transform Check;
     public LayerMask mask;
     // Start is called before the first frame update
     void Start()
     {
-        
+       Jumpscare.SetActive(false);
+        asbeenjumpscared = false;
     }
 
     // Update is called once per frame
@@ -22,7 +27,18 @@ public class GoToPlayer : MonoBehaviour
         bool isnear = Physics.CheckSphere(Check.position, 2, mask);
         if (isnear)
         {
-            Debug.Break();
+            asbeenjumpscared = true;
+            Jumpscare.SetActive(true);
+        }
+
+        if (asbeenjumpscared) { 
+            countdown = countdown + Time.deltaTime;
+            if (countdown > 5)
+            {
+                SceneManager.LoadScene("Menu");
+                Cursor.lockState = CursorLockMode.Confined;
+                Cursor.visible = true;
+            }
         }
     }
 }
