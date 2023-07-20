01940077cbe0542e10f13e82ba4ee1866dba49ef
tbvns <69420062+tbvns25565@users.noreply.github.com>
2023-07-20 13:44:04 +0000

    Fixed item count going below 0

--- UnityGameTest/Assets/GameCode/Inventory/DisplayInventory.cs
+++ UnityGameTest/Assets/GameCode/Inventory/DisplayInventory.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using Unity.VisualScripting;
+using System.Linq;
 
 public class DisplayInventory : MonoBehaviour
 {
@@ -35,6 +37,44 @@ public class DisplayInventory : MonoBehaviour
             ItemDisplayed.Add(inventory.Container[i], OBJ);
         }
     }
+
+    public void ReloadDisplay()
+    {
+        List<GameObject> UIToDestroy = new List<GameObject>();
+        List<InventorySlot> SlotToDestroy = new List<InventorySlot>();
+        
+        foreach(var item in ItemDisplayed)
+        {
+            InventorySlot slot = item.Key;
+            GameObject UI = item.Value.gameObject;
+
+            UIToDestroy.Add(UI);
+            SlotToDestroy.Add(slot);
+            
+        }
+
+        foreach(InventorySlot slot in SlotToDestroy)
+        {
+            ItemDisplayed.Remove(slot);
+        }
+
+        foreach(GameObject UI in UIToDestroy)
+        {
+            Destroy(UI);
+        }
+
+        UIToDestroy.Clear();
+        SlotToDestroy.Clear();
+
+        for (int i = 0; i < inventory.Container.Count; i++)
+        {
+            var OBJ = Instantiate(inventory.Container[i].item.prefab, Vector3.zero, Quaternion.identity, transform);
+            OBJ.GetComponent<RectTransform>().localPosition = GetPosition(i);
+            OBJ.GetComponentInChildren<TextMeshProUGUI>().text = inventory.Container[i].amount.ToString("n0");
+            ItemDisplayed.Add(inventory.Container[i], OBJ);
+        }
+    }
+
     public void UpdateDisplay()
     {
         for (int i = 0; i < inventory.Container.Count; i++)
@@ -50,10 +90,16 @@ public class DisplayInventory : MonoBehaviour
                 OBJ.GetComponentInChildren<TextMeshProUGUI>().text = inventory.Container[i].amount.ToString("n0");
                 ItemDisplayed.Add(inventory.Container[i], OBJ);
             }
+            if (inventory.Container[i].amount <= 0)
+            {
+                inventory.Container.RemoveAt(i);
+                ReloadDisplay();
+            }
         }
     }
     public Vector3 GetPosition(int i)
     {
         return new Vector3(DefaultPosition.x + (X_Space_BETWEEN_ITEM * (i % NumberOfItemInColumn)), DefaultPosition.y + ((-Y_Space_BETWEEN_ITEM * (i / NumberOfItemInColumn))), 0f);
     }
+    
 }
